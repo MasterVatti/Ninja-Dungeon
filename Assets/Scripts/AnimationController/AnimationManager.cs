4a52005ba19b177d6f810997b1c9d42d7edccd6f
Citizen-Oleg <oleg-12-00@bk.ru>
2021-03-26 18:42:55 +0300

    Finish

---
+++ Assets/Scripts/AnimationController/AnimationManager.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using ResourceSystem;
+using UnityEngine;
+
+/// <summary>
+/// Класс отвечает за анимацию передачи ресурсов.
+/// </summary>
+
+public class AnimationManager : Singleton<AnimationManager>
+{
+    [SerializeField]
+    private float _flightTime;
+    [SerializeField]
+    private List<ResoursePrefab> _resourсePrefab;
+    [SerializeField]
+    private AnimationCurve _yPositionCurve;
+    
+    private List<AnimationInformation> _animationInformations;
+    private Dictionary<ResourceType, ObjectPool> _resourcePool;
+    
+    private void Start()
+    {
+        _animationInformations = new List<AnimationInformation>();
+        _resourcePool = new Dictionary<ResourceType, ObjectPool>();
+    }
+    
+    private void Update()
+    {
+        for (int i = 0; i < _animationInformations.Count; i++)
+        {
+            var information = _animationInformations[i];
+            information.Progress += Time.deltaTime /  _flightTime;
+            var positionYcurve = _yPositionCurve.Evaluate(information.Progress) * Vector3.up;
+
+            information.PrefabResource.transform.position = Vector3.Lerp
+                (information.StartPoint, information.EndPoint, information.Progress) + positionYcurve;
+
+            if (information.Progress >= 1)
+            {
+                information.PrefabResource.SetActive(false);
+                _animationInformations.RemoveAt(i);
+            }
+        }
+    }
+    
+    public void ShowFlyingResource(ResourceType resourceType, Vector3 source, Vector3 destination)
+    {
+        if (!_resourcePool.TryGetValue(resourceType, out var objectPool))
+        {
+            objectPool = new ObjectPool(GetResourcePrefab(resourceType),10);
+            _resourcePool.Add(resourceType,objectPool);
+        }
+        var information = new AnimationInformation
+        {
+            PrefabResource = objectPool.Get(),
+            StartPoint = source,
+            EndPoint = destination,
+            Progress = 0
+        };
+        _animationInformations.Add(information);
+    }
+    
+    private GameObject GetResourcePrefab(ResourceType resourceType)
+    {
+        foreach (var resourcePrefab in _resourсePrefab)
+        {
+            if (resourcePrefab.Type == resourceType)
+            {
+                return resourcePrefab.Prefab;
+            }
+        }
+        return null;
+    }
+}
