65aa890034b4b5ccb5bef208aab995cf842f343e
straf3ee <straf3ee@inbox.ru>
2021-04-04 16:10:25 +0300

    Added

---
+++ Assets/Scripts/EnemyAI(Behavior Tree)/EnemyProjectile.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    [SerializeField] 
+    private int _damage;
+        
+    [SerializeField]
+    private float _timeToRemove = 5f;
+        
+    private Rigidbody _rigidbody;
+        
+    private void Awake()
+    {
+        Destroy(gameObject, _timeToRemove);
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
