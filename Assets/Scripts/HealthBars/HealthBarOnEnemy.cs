656fca4e15ec709f2576b1f3a58c5ea780d28ef7
VanooS123 <ivanstulskij@gmail.com>
2021-04-18 15:28:43 +0300

    Divide HealthBar on Enemy and HealthBehavior on two parts

---
+++ Assets/Scripts/HealthBars/HealthBarOnEnemy.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Enemies;
+using HealthBars;
+using UnityEngine;
+
+public class HealthBarOnEnemy : MonoBehaviour
+{
+    [SerializeField]
+    private HealthBarsManager _healthBarsManager;
+    
+    private HealthBar _healthBar;
+    private HealthBehavior _healthBehavior;
+    private int _health;
+    
+    private void Awake()
+    {
+        _healthBehavior = GetComponent<HealthBehavior>();
+        _health = _healthBehavior.Health;
+        _healthBar = _healthBarsManager.CreateHealthBar();
+        _healthBar.SetMaximalHealth(_health);
+        _healthBar.StabilizatePosition(transform.position);
+        _healthBehavior.HealthBarValueDecrease += SetValueOnHealthBar;
+    }
+    
+    private void Update()
+    {
+        _healthBar.StabilizatePosition(transform.position);
+    }
+
+    private void OnDestroy()
+    {
+        _healthBehavior.HealthBarValueDecrease -= SetValueOnHealthBar;
+    }
+
+    public void SetValueOnHealthBar(int health)
+    {
+        _healthBar.SetHealthBarValue(health);
+        if (health <= 0)
+        {
+            _healthBar.Destroy();
+        }
+    }
+}
