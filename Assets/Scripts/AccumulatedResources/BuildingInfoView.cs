ff5861461c9e5108dfd349af1b6e9cec6866b2c5
Nikita Papkov <nikita@wgames.com>
2021-04-20 22:35:19 -0500

    fixed buildings ui

--- Assets/Scripts/AccumulatedResources/BuildingInfoView.cs
+++ Assets/Scripts/AccumulatedResources/BuildingInfoView.cs
@@ -9,6 +9,16 @@ public class BuildingInfoView : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI _nameBuilding;
 
+    private Transform _attachPoint;
+    private RectTransform _parent;
+    private RectTransform _currentTransform;
+
+    private void Awake()
+    {
+        _currentTransform = transform as RectTransform;
+        _parent = transform.parent as RectTransform;
+    }
+
     private void ShowNameBuilding(string nameBuilding)
     {
         _nameBuilding.text = nameBuilding;
@@ -16,9 +26,26 @@ public class BuildingInfoView : MonoBehaviour
     
     public virtual void Initialize(GameObject building, Transform uiAttachPoint, string nameBuilding)
     {
-        transform.position = uiAttachPoint.position;
-        transform.rotation = Quaternion.identity;
-        
+        _attachPoint = uiAttachPoint;
         ShowNameBuilding(nameBuilding);
+        UpdateViewPosition(_attachPoint);
+    }
+
+    protected virtual void Update()
+    {
+        if (_attachPoint != null)
+        {
+            UpdateViewPosition(_attachPoint);
+        }
+    }
+
+    private void UpdateViewPosition(Transform attachPoint)
+    {
+        var worldPoint = attachPoint.transform.position;
+        
+        var screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, worldPoint);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_parent, screenPoint, null,
+            out var localPoint);
+        _currentTransform.anchoredPosition = localPoint;
     }
 }
