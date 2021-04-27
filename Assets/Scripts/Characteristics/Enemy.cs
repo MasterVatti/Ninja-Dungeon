34a8f0dbe53353a4c44377e197ec70c4aec84866
Citizen-Oleg <oleg-12-00@bk.ru>
2021-04-28 00:42:23 +0300

    Опять забыл удалить лишнее

--- Assets/Scripts/Characteristics/Enemy.cs
+++ Assets/Scripts/Characteristics/Enemy.cs
@@ -4,9 +4,5 @@ namespace Characteristics
 {
     public class Enemy : Person
     {
-        public EnemyCharacteristics EnemyCharacteristics => _enemyCharacteristics;
-
-        [SerializeField]
-        private EnemyCharacteristics _enemyCharacteristics;
     }
 }
