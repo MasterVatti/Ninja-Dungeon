779bb94dbbcb9c6c2488ea0f5c1066f2ed4cbea4
APBorK <1659732@gmail.com>
2021-05-30 12:37:44 +0300

    Исправил код

--- Assets/Scripts/ExperienceSystem/LevelTolerance.cs
+++ Assets/Scripts/ExperienceSystem/LevelTolerance.cs
@@ -2,33 +2,40 @@ using System;
 using Characteristics;
 using UnityEngine;
 
-public class LevelTolerance : MonoBehaviour
-{
-    // Доступ к сценам по уровню
+/// Доступ к сценам по уровню
+
+namespace ExperienceSystem
+{ 
+    [RequireComponent(typeof(PlayerCharacteristics))]
+    public class LevelTolerance : MonoBehaviour
+    {
+        
     
-    [SerializeField]
-    private GameObject _teleportScene;
-    [SerializeField]
-    private int _levelUnlock;
-    private PlayerCharacteristics _playerCharacteristics;
+        [SerializeField]
+        private GameObject _teleportScene;
+        [SerializeField]
+        private int _levelUnlock;
+        private PlayerCharacteristics _playerCharacteristics;
 
-    private void Start()
-    {
-        UnlockLevel(_playerCharacteristics.LevelUpperWorld);
-        MainManager.Player.ExperienceControllerUpperWorld.OnLevelUp += UnlockLevel;
-    }
-
-    private void UnlockLevel(int level)
-    {
-        if (_levelUnlock <= level)
+        private void Start()
         {
-            _teleportScene.SetActive(true);
+            UnlockLevel(_playerCharacteristics.LevelUpperWorld);
+            MainManager.Player.ExperienceControllerUpperWorld.OnLevelUp += UnlockLevel;
         }
-        _teleportScene.SetActive(false);
-    }
 
-    private void OnDestroy()
-    {
-        MainManager.Player.ExperienceControllerUpperWorld.OnLevelUp -= UnlockLevel;
+        private void UnlockLevel(int level)
+        {
+            if (_levelUnlock <= level)
+            {
+                _teleportScene.SetActive(true);
+            }
+            _teleportScene.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            MainManager.Player.ExperienceControllerUpperWorld.OnLevelUp -= UnlockLevel;
+        }
     }
 }
+
