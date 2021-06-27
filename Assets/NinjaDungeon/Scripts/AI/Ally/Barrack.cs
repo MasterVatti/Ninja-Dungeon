01e0c8de035c75e415f19c43b1aeba58d806a812
IvaxaMaks <76532964+Ivaxamaks@users.noreply.github.com>
2021-06-27 12:15:19 +0300

    Customization

--- Assets/NinjaDungeon/Scripts/AI/Ally/Barrack.cs
+++ Assets/NinjaDungeon/Scripts/AI/Ally/Barrack.cs
@@ -3,7 +3,6 @@ using Assets.Scripts.Managers.ScreensManager;
 using BuildingSystem;
 using SaveSystem;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Barracks_and_allied_behavior
 {
@@ -15,18 +14,15 @@ namespace Barracks_and_allied_behavior
     public class Barrack : Building<BarrackData>, IScreenOpenerWithContext
     {
         public List<AlliesSetting> Allies => _allies;
-        public bool IsAllyCreated => _createdAlly != null;
         
         [SerializeField]
         private List<AlliesSetting> _allies;
         [SerializeField]
         private Transform _spawnPoint;
-
-        private GameObject _createdAlly;
         
         public void CreateAlly(AlliesSetting ally)
         {
-            _createdAlly = Instantiate(ally.AllyPrefab, _spawnPoint.position, Quaternion.identity);
+            Instantiate(ally.AllyPrefab, _spawnPoint.position, Quaternion.identity);
         }
         
         public void ShowScreenWithContext()
