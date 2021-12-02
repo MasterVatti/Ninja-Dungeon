4924678ba11c50c569bc8698d4246922ddb26157
Citizen-Oleg <oleg-12-00@bk.ru>
2021-12-02 11:08:47 +0300

    Door opening depending on the level

---
+++ Assets/NinjaDungeon/Scripts/BattleManager/LevelLogic/DoorOpener.cs
@@ -0,0 +1,22 @@
+using Characteristics;
+using UnityEngine;
+
+namespace NinjaDungeon.Scripts.BattleManager.LevelLogic
+{
+    public class DoorOpener : MonoBehaviour
+    {
+        [SerializeField]
+        private int _requiredLevel;
+        [SerializeField]
+        private BoxCollider _boxCollider;
+        
+        private void Start()
+        {
+            var playerCharacteristics = (PlayerCharacteristics) MainManager.Player.PersonCharacteristics;
+            if (playerCharacteristics.LevelUpperWorld >= _requiredLevel)
+            {
+                _boxCollider.isTrigger = false;
+            }
+        }
+    }
+}
